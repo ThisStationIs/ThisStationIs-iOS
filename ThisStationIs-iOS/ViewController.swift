89753bbb2dcd4b6611ffc402cecc3a3378fcbfae
min <75626237+mingging@users.noreply.github.com>
2023-04-17 20:51:00 +0900

    Initial Commit

---
+++ ThisStationIs-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThisStationIs-iOS
+//
+//  Created by min on 2023/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
