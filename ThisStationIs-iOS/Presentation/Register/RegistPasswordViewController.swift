43ec16bdce8e03851a347ad7a06b60bcd5ee5917
simyoju <dawnofspring@daum.net>
2023-06-07 01:55:23 +0900

    [Feat] 회원가입: 비밀번호 설정 UI 레이아웃 잡는중

--- ThisStationIs-iOS/Presentation/Register/RegistPasswordViewController.swift
+++ ThisStationIs-iOS/Presentation/Register/RegistPasswordViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
+import FlexLayout
+
 class RegistPasswordViewController: UIViewController {
     let mainDescriptionLabel = UILabel().then {
         $0.text = "로그인에 사용할\n비밀번호를 입력해주세요."
+        $0.textColor = AppColor.setupColor(.textMain)
+        $0.backgroundColor = .red
     }
     
     let passwordInputView = InputView(label: "비밀번호", placeholder: "비밀번호를 입력해주세요.").then {
@@ -18,5 +22,33 @@ class RegistPasswordViewController: UIViewController {
     
     let checkPasswordInputView = InputView(label: "비밀번호 확인", placeholder: "비밀번호를 다시 입력해주세요.").then {
         $0.setRightViewMode(type: .auth)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupLayout()
+    }
+}
+
+extension RegistPasswordViewController {
+    private func setupView(){
+        view.backgroundColor = .white
+        
+        [
+            mainDescriptionLabel,
+            passwordInputView,
+            checkPasswordInputView
+        ].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupLayout(){
+        mainDescriptionLabel.pin.top(view.safeAreaInsets.top + 73)
     }
 }
