32dbcee8504455a81600b8eb62225702653cf266
simyo <simyo@newlinkcorp.co.kr>
2023-06-07 11:43:50 +0900

    [Feat] 회원가입: 즐겨찾는 호선 선택 화면 - 호선 컬렉션 뷰 생성

--- ThisStationIs-iOS/Presentation/Register/SelectFavoriteLineViewController.swift
+++ ThisStationIs-iOS/Presentation/Register/SelectFavoriteLineViewController.swift
@@ -14,6 +14,10 @@ class SelectFavoriteLineViewController: UIViewController {
         $0.numberOfLines = 0
     }
     
+    let lineCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .red
+    }
+    
     let bottomButton = MainButton().then {
         $0.setTitle("가입 완료", for: .normal)
     }
@@ -35,6 +39,7 @@ extension SelectFavoriteLineViewController {
         
         [
             mainDescriptionLabel,
+            lineCollectionView,
             bottomButton
         ].forEach {
             view.addSubview($0)
@@ -45,6 +50,16 @@ extension SelectFavoriteLineViewController {
         mainDescriptionLabel.sizeToFit()
         mainDescriptionLabel.pin.top(view.safeAreaInsets.top + 73)
             .left(24)
+        
+        lineCollectionView.pin
+            .below(of: mainDescriptionLabel)
+            .above(of: bottomButton)
+            .horizontally(24)
+//            .marginTop(40)
+            .marginVertical(40)
+//            .marginBottom(97)
+            .height(200)
+            
         
         bottomButton.pin.bottom(view.safeAreaInsets.bottom + 34)
             .horizontally(24)
