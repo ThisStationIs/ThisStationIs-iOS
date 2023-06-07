450c3f38f5095f075d8d3ee22a4d7f1925238223
simyo <simyo@newlinkcorp.co.kr>
2023-06-07 11:24:54 +0900

    [Feat] 회원가입: 비밀번호 설정 페이지 메인 설명 라벨 레이아웃 설정

--- ThisStationIs-iOS/Presentation/Register/RegistPasswordViewController.swift
+++ ThisStationIs-iOS/Presentation/Register/RegistPasswordViewController.swift
@@ -11,9 +11,9 @@ import FlexLayout
 
 class RegistPasswordViewController: UIViewController {
     let mainDescriptionLabel = UILabel().then {
-        $0.text = "로그인에 사용할\n비밀번호를 입력해주세요."
         $0.textColor = AppColor.setupColor(.textMain)
-        $0.backgroundColor = .red
+        $0.attributedText = .attributeFont(font: .heading24, text: "로그인에 사용할\n비밀번호를 입력해주세요.")
+        $0.numberOfLines = 0
     }
     
     let passwordInputView = InputView(label: "비밀번호", placeholder: "비밀번호를 입력해주세요.").then {
@@ -49,6 +49,8 @@ extension RegistPasswordViewController {
     }
     
     private func setupLayout(){
+        mainDescriptionLabel.sizeToFit()
         mainDescriptionLabel.pin.top(view.safeAreaInsets.top + 73)
+            .left(24)
     }
 }
