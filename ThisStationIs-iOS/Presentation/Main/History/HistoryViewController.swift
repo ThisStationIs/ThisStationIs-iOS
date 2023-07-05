0d76593f52658d7940e2aefee141db3ee3fffd04
simyo <dawnofspring@kakao.com>
2023-07-05 13:49:13 +0900

    [Feat] 내활동: 테이블뷰 레이아웃 개발 및 noDataCell 등록

--- ThisStationIs-iOS/Presentation/Main/History/HistoryViewController.swift
+++ ThisStationIs-iOS/Presentation/Main/History/HistoryViewController.swift
@@ -6,24 +6,70 @@
 //
 
 import UIKit
+import Then
+import FlexLayout
+
+enum CellType {
+    case isData
+    case noData
+}
 
 class HistoryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let historyTableView = UITableView().then {
+        $0.register(NoticeNoDataTableViewCell.self, forCellReuseIdentifier: "NoticeNoDataTableViewCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var cellType: CellType = .isData
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "알림"
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupLayout()
+    }
+}
 
+extension HistoryViewController {
+    private func setupView(){
+        view.addSubview(historyTableView)
+        historyTableView.backgroundColor = .yellow
+        historyTableView.delegate = self
+        historyTableView.dataSource = self
+    }
+    
+    private func setupLayout(){
+        historyTableView.pin.top(view.safeAreaInsets.top) // 네비게이션 바 아래로 상단 여백을 설정합니다.
+            .all() // 나머지 영역을 화면 상하좌우에 맞춥니다.
+        
+        view.flex.direction(.column).define { flex in
+            flex.addItem(historyTableView)
+                .grow(1)
+            // tableViewA를 화면 전체 영역에 늘어나도록 설정합니다.
+        }
+    }
+}
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellType == .noData ? 1 : 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoticeNoDataTableViewCell", for: indexPath) as! NoticeNoDataTableViewCell
+        cell.setupText(text: "알림이 존재하지 않습니다.")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellType == .noData ? tableView.frame.height : 102
+    }
 }
