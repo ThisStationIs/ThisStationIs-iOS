16ad72994630056d8be06d22ef8e7f8e02a7b98d
simyoju <dawnofspring@daum.net>
2023-08-15 01:47:42 +0900

    [Feat] 마이: 메뉴용셀 적용

--- ThisStationIs-iOS/Presentation/Main/My/MyViewController.swift
+++ ThisStationIs-iOS/Presentation/Main/My/MyViewController.swift
@@ -10,10 +10,15 @@ import Then
 
 class MyViewController: UIViewController {
     private let myTableView = UITableView().then {
-        $0.backgroundColor = .blue
+        $0.backgroundColor = .white
+        $0.separatorStyle = .none
         $0.register(MyTableViewHeader.self, forHeaderFooterViewReuseIdentifier: "MyTableViewHeader")
         $0.register(MyTitleCell.self, forCellReuseIdentifier: "MyTitleCell")
+        $0.register(MyMenuCell.self, forCellReuseIdentifier: "MyMenuCell")
     }
+    
+    private let myActivityMenus: [String] = ["내 활동", "내가 쓴 글", "내가 쓴 댓글"]
+    private let thisStationIsMenus: [String] = ["이번역은", "문의하기"]
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -78,11 +83,11 @@ extension MyViewController {
 extension MyViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "MyTableViewHeader") as! MyTableViewHeader
-        return header
+        return section == 0 ? header : nil
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return section == 0 ? 88 : 0
+        return section == 0 ? 88 : 40
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -94,15 +99,21 @@ extension MyViewController: UITableViewDelegate, UITableViewDataSource {
         let row = indexPath.row
         
         let titleCell = tableView.dequeueReusableCell(withIdentifier: "MyTitleCell") as! MyTitleCell
-        titleCell.setupTitle(title: section == 0 ? "내 활동" : "이번역은")
-        let menuCell = UITableViewCell()
+        titleCell.setupTitle(title: section == 0 ? myActivityMenus[0] : thisStationIsMenus[0])
+        
+        let menuCell = tableView.dequeueReusableCell(withIdentifier: "MyMenuCell") as! MyMenuCell
+        menuCell.setupTitle(title: section == 0 ? myActivityMenus[row] : thisStationIsMenus[row])
         
         return row == 0 ? titleCell : menuCell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return section == 0 ? myActivityMenus.count : thisStationIsMenus.count
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let row = indexPath.row
+        return row == 0 ? 45 : 40
+    }
     
 }
